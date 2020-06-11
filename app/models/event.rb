6a4da25c6b54ded68c283290ae0aad0f6f15af44
OdongAlican <sandieo.2020@gmail.com>
2020-06-11 16:55:50 +0300

    adds association model between events and attendees

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
-    belongs_to :creator, class_name: 'User'
+    belongs_to :creator, class_name: "User"
+    has_many :event_attendings, foreign_key: :attended_event_id, dependent: :destroy
+    has_many :attendees, through: :event_attendings, source: :attendee
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_11_110503) do
+ActiveRecord::Schema.define(version: 2020_06_11_134631) do
+
+  create_table "event_attendings", force: :cascade do |t|
+    t.integer "attended_event_id"
+    t.integer "attendee_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "title"
